3ca411dc7ea8427d4a4be7106230d656308bcf32
ystays <ystay@g.ucla.edu>
2023-10-10 17:30:21 +0800

    Added profile photo and card to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+import Card from "@/components/home/card"
+import ComponentGrid from "@/components/home/component-grid"
+import WebVitals from "@/components/home/web-vitals"
+
 export default async function About() {
     return(
         <div className="z-10 w-full max-w-xl px-5 xl:px-0">
@@ -21,6 +25,35 @@ export default async function About() {
       <section>
         <h2>In progress...</h2>
       </section>
+
+      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
+        {features.map(({ title, description, demo, large, fullWidth }) => (
+          <Card
+            key={title}
+            title={title}
+            description={description}
+            demo={
+              title === "Beautiful, reusable components" ? (
+                <ComponentGrid />
+              ) : (
+                demo
+              )
+            }
+            large={large}
+            fullWidth={fullWidth}
+          />
+        ))}
+      </div>
+
       </div>
     )
 }
+
+const features = [{
+    title: "...",
+    description:
+      "Words, words, words",
+    demo: <img alt="profile photo" src="/profile.jpg" width={100} height={100}></img>,
+    large: false,
+    fullWidth: true,
+},]
