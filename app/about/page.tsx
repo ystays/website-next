8ca5f115c3a5d67a033f767140ac5b3a49fca0b9
ystays <ystay@g.ucla.edu>
2023-10-10 17:16:03 +0800

    Added some text to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,26 @@
 export default async function About() {
     return(
-        <div>
-            <h1>About</h1>
-        </div>
+        <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+      <section>
+        <p>Hi.
+          I'm a 2023 Computer Science graduate from UCLA.</p>
+        <small>
+          (This is my personal website. Check out {' '}
+          <a href="https://linkedin.com/in/yi-sheng-tay">my LinkedIn page</a>.)
+        </small>
+        <br/>
+        <br/>
+        <small>
+          Recent Computer Science graduate from UCLA, with an interest in backend development, AI and digital humanities. Previously interned at Intel and Keysight, and has lately been exploring building web apps with the MERN stack. 
+          <br/>
+          <br/>
+          Searching for a new grad SWE role. Looking for stimulating, collaborative work with a supportive team.
+        </small>
+      </section>
+      <br/>
+      <section>
+        <h2>In progress...</h2>
+      </section>
+      </div>
     )
 }
