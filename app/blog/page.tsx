dba5e0165848178f97d4052a7796f27b798d6f19
ys <85421593+ystays@users.noreply.github.com>
2026-02-06 22:12:58 +0800

    (fix): Fix disappearing post previews

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,41 +1,31 @@
-// "use client";
 
-import Card from "@/components/home/card"
-import ComponentGrid from "@/components/home/component-grid"
-import WebVitals from "@/components/home/web-vitals"
-// import Image from "next/image"
-import Image from "@/components/home/image-card"
 
 import getPostMetadata from "../../components/posts/getPostMetadata";
 import PostPreview from "../../components/posts/PostPreview";
 import FilterPopover from "@/components/blog/filter-popover";
 
-export default async function Blog() {
-    const postMetadata = getPostMetadata();
-    const postPreviews = postMetadata.map((post) => (
-      <PostPreview key={post.slug} {...post} />
-    ));
+export default function Blog() {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((post) => (
+    <PostPreview key={post.slug} {...post} />
+  ));
 
-    return(
-      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <h1
-            className="animate-fade-up bg-linear-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-xs text-balance md:text-7xl md:leading-20"
-            style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-        >
-          Blog
-        </h1>
-        <br/>
-        <div className="flex justify-end"><FilterPopover currentTag={ "Tags" }></FilterPopover></div>
-        <div>
-        {/* <div className="z-10 w-full max-w-xl px-5 xl:px-0 sm:max-w-fit"> */}
-          {/* <div
-              className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
-              style={{ animationDelay: "0.3s", animationFillMode: "forwards" }} 
-              >*/}
-      <div className="my-10 grid w-full max-w-(--breakpoint-lg) animate-fade-up grid-cols-1 gap-5 px-5 lg:grid-cols-2 xl:px-0 place-items-center">
-            {postPreviews}
-          </div>
-        </div>
+  return (
+    <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+      <h1
+        className="animate-fade-up bg-linear-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-xs text-balance md:text-7xl md:leading-20"
+        style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+      >
+        Blog
+      </h1>
+      <br />
+      <div className="flex justify-end"><FilterPopover currentTag={"Tags"}></FilterPopover></div>
+      <div
+        className="my-10 grid w-full max-w-(--breakpoint-lg) animate-fade-up grid-cols-1 gap-5 px-5 lg:grid-cols-2 xl:px-0 place-items-center"
+        style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+      >
+        {postPreviews}
       </div>
-    )
+    </div>
+  )
 }
--- components/shared/popover.tsx
+++ components/shared/popover.tsx
@@ -22,8 +22,7 @@ export default function Popover({
   if (isMobile) {
     return (
       <Drawer.Root open={openPopover} onOpenChange={setOpenPopover}>
-        {/* <div className="sm:hidden">{children}</div> */}
-        <div>{children}</div>
+        <div className="sm:hidden">{children}</div>
         <Drawer.Overlay className="fixed inset-0 z-40 bg-gray-100 bg-opacity-10 backdrop-blur-sm" />
         <Drawer.Portal>
           <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 mt-24 rounded-t-[10px] border-t border-gray-200 bg-white">
